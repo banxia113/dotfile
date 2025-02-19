02d0148aa2281161b685e0248996bf2e14c72005
banxia113 <2747990410@qq.com>
2025-02-19 19:29:08 +0800

    first-dotfiles

---
+++ dotfile/nvim/lua/user/lsp/settings/clangd.lua
@@ -0,0 +1,16 @@
+return {
+  setting = {
+    clangd = {
+      diagnostics = {
+        Limited = 0,
+      },
+      completion = {
+        auto = true,
+      },
+      formatting = {
+        FormatStyle = "LLVM",
+        SortIncludes = true,
+      },
+    },
+  },
+}
---
+++ dotfile/nvim/lua/user/lsp/settings/lua_ls.lua
@@ -0,0 +1,15 @@
+return {
+  setting = {
+    Lua = {
+      diagnostics = {
+        globals = {"vim"},
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.stdpath("config") .. "/lua"] = true,
+        },
+      },
+    },
+  },
+}
